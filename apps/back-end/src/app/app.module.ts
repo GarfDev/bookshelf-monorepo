a2c4df2b27f5ca982e87300e5f8300a8a2496a43
GarfDev <garfdev.13@gmail.com>
2022-01-18 17:04:44 +0700

    feat: initialize auth service
    Planned to use Passport as it have strong community support and officialy supported by NestJs

--- apps/back-end/src/app/app.module.ts
+++ apps/back-end/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URL), UsersModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGO_URL), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
